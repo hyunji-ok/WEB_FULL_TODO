145a7ce35091f61165f9b0390804c12a893251ce
hyunji-ok <61300084+hyunji-ok@users.noreply.github.com>
2021-11-17 13:22:12 +0900

    todo 백엔드 CRUD 구현

--- todo/spring/demo/src/main/java/com/example/demo/dto/TodoDTO.java
+++ todo/spring/demo/src/main/java/com/example/demo/dto/TodoDTO.java
@@ -1,13 +1,10 @@
 package com.example.demo.dto;
 
 import com.example.demo.model.TodoEntity;
-
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
@@ -15,10 +12,18 @@ public class TodoDTO {
 	private String id;
 	private String title;
 	private boolean done;
-	
+
 	public TodoDTO(final TodoEntity entity) {
 		this.id = entity.getId();
 		this.title = entity.getTitle();
 		this.done = entity.isDone();
+	}
+
+	public static TodoEntity toEntity(final TodoDTO dto) {
+		return TodoEntity.builder()
+						.id(dto.getId())
+						.title(dto.getTitle())
+						.done(dto.isDone())
+						.build();
 	}
 }
