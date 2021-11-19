d0ea3b18680eec6c47042b9c6e734a2d8e11b979
hyunji-ok <61300084+hyunji-ok@users.noreply.github.com>
2021-11-19 10:15:29 +0900

    todo 추가 구현

--- todo/react/todo-react-app/todo-react-app/src/App.js
+++ todo/react/todo-react-app/todo-react-app/src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Todo from "./Todo";
-import {Paper, List} from "@material-ui/core"
+import AddTodo from "./AddTodo";
+import {Paper, List, Container} from "@material-ui/core"
 import './App.css';
 
 class App extends React.Component {
@@ -14,6 +15,17 @@ class App extends React.Component {
       };
   }
 
+  // (1) 함수 추가
+  add = (item) => {
+    const thisItems = this.state.items;
+    item.id = "ID-" + thisItems.length; // key를 위한 id 추가
+    item.done = false; // done 초기화
+    thisItems.push(item);
+    this.setState({ items: thisItems });
+    console.log("items : ", this.state.items);
+  }
+
+
   render() {
     var todoItems = this.state.items.length > 0 && (
       <Paper style={{margin:16}}>
@@ -25,8 +37,15 @@ class App extends React.Component {
       </Paper>
     )
     
-    return <div className="App">{todoItems}</div>;
-    
+    // (2) 함수 연결
+    return (
+      <div className="App">
+        <Container maxWidth="md">
+          <AddTodo add={this.add} />
+          <div className="TodoList">{todoItems}</div>
+        </Container>
+      </div>
+    );
   }
 }
 
