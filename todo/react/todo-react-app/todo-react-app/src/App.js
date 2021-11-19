d405c58f582186b2543ad14790843607d261dac3
hyunji-ok <61300084+hyunji-ok@users.noreply.github.com>
2021-11-19 16:07:44 +0900

    todo CRUD 구현

--- todo/react/todo-react-app/todo-react-app/src/App.js
+++ todo/react/todo-react-app/todo-react-app/src/App.js
@@ -1,43 +1,48 @@
 import React from "react";
 import Todo from "./Todo";
-import AddTodo from "./AddTodo";
-import {Paper, List, Container} from "@material-ui/core"
-import './App.css';
+import AddTodo from "./AddTodo.js";
+import { Paper, List, Container } from "@material-ui/core";
+import "./App.css";
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      items: [
-          {id: "0", title: "Hello World 1", done: true },
-          {id: "1", title: "Hello World 2", done: false },
-        ],
-      };
+      items: [],
+    };
   }
 
-  // (1) 함수 추가
   add = (item) => {
     const thisItems = this.state.items;
-    item.id = "ID-" + thisItems.length; // key를 위한 id 추가
+    item.id = "ID-" + thisItems.length; // key를 위한 id추가
     item.done = false; // done 초기화
-    thisItems.push(item);
-    this.setState({ items: thisItems });
+    thisItems.push(item); // 배열에 아이템 추가
+    this.setState({ items: thisItems }); // 업데이트는 반드시 this.setState로 해야됨.
     console.log("items : ", this.state.items);
-  }
+  };
 
+  delete = (item) => {
+    const thisItems = this.state.items;
+    console.log("Before Update Items : ", this.state.items);
+    const newItems = thisItems.filter((e) => e.id !== item.id); // 해당 id 걸러내기
+    this.setState({ items: newItems }, () => {
+      // 디버깅 콜백
+      console.log("Update Items : ", this.state.items);
+    });
+  };
 
   render() {
     var todoItems = this.state.items.length > 0 && (
-      <Paper style={{margin:16}}>
+      <Paper style={{ margin: 16 }}>
         <List>
           {this.state.items.map((item, idx) => (
-            <Todo item={item} key={item.id} />
+            <Todo item={item} key={item.id} delete={this.delete} />
           ))}
         </List>
       </Paper>
-    )
-    
-    // (2) 함수 연결
+    );
+
+    // 3. props로 넘겨주기
     return (
       <div className="App">
         <Container maxWidth="md">
